44dc8ddc456bd4d4671d3cc6b49e8888faef273a
Diwakartak <diwakar@Diwakartaks-Mac-mini.local>
2021-01-21 21:03:21 +0530

    first commit

---
+++ test/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Diwakartak on 21/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("hello world")
+    }
+
+
+}
+
